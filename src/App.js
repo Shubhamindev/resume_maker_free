0565882e808c9a4e9055f190fc29e5aec54f6602
eklotaravan <eklotaravan@gmail.com>
2023-12-14 18:47:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import './App.css';
+import ResumeState from './Context/ResumeState';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Pages/About/About';
+
+function App() {
+  return (
+    <ResumeState>
+      <div className="App">
+        <Helmet>
+          <title>Resume Builder - Create Professional Resumes Online</title>
+          <meta name="description" content="Build and customize professional resumes online with Resume Builder. Choose from a variety of templates and create your perfect resume easily." />
+          <meta name="keywords" content="resume builder, professional resumes, online resumes, resume templates" />
+          
+        </Helmet>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </ResumeState>
+  );
+}
+
+export default App;
